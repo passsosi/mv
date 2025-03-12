d0e316be8e4c4125636e755fefd86f8cdea8501b
passosi <pakinpok>
2025-03-12 14:30:44 +0900

    v11

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Virtual Museum</title>
+    <link rel="icon" href="{{ asset('/images/favicon.ico') }}" type="image/x-icon">
+    <title>Виртуальный атлас</title>
 </head>
 
 <body>
